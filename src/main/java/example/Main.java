fddae3bdd3339959a91be426e252809dfdb72268
nadazaghdoud <nadazaghdoud.5@gmail.com>
2024-02-10 16:25:45 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/example/Main.java

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -14,30 +14,6 @@ import static gestion_finance.entities.Type.*;
 public class Main {
     public static void main(String[] args) {
 
-       /* DataSource ds1 = DataSource.getInstance();
-        System.out.println(ds1);*/
-        AbonnementService as = new AbonnementService();
-        Abonnement abonnement1 = new Abonnement(
-                2, mensuel , 100.0,
-                Date.valueOf("2024-02-09"),
-                Date.valueOf("2024-03-09") ,
-               actif,
-                1,1
-        );
-        Abonnement abonnement2 = new Abonnement(
-                2, mensuel , 100.0,
-                Date.valueOf("2024-02-09"),
-                Date.valueOf("2024-03-09") ,
-                actif,
-                2,1
-        );
-
-        /*as.add(abonnement1);*/
-        as.add(abonnement2);
-        //as.delete(abonnement1);
-        as.readAll().forEach(System.out::println);
-        //System.out.println("L'abonnement a été ajouté avec succès !");
-
 
 
 
